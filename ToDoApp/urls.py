78c98b2b621f40b7e51b470e86cae26932252d71
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-20 17:12:33 +0330

    Represented ListCreate view and it's path

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    #path('todo/', include("ToDoApplication.urls")),
+    path('todo/', include("ToDoApplication.urls")),
 ]
--- ToDoApplication/views.py
+++ ToDoApplication/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import todo
+from .serializers import TodoSerializers
+
+
+class TodoView(generics.ListCreateAPIView):
+    queryset = todo.objects.all()
+    serializer_class = TodoSerializers
