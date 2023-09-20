f4548ed56b5ca69ae7d6e44be0cf2e284c2e25b6
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-20 13:02:39 +0330

    added todo model

--- ToDoApplication/models.py
+++ ToDoApplication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class todo(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    priority = models.IntegerField(default=1)
+    is_done = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'todos'
